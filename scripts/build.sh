1d6634e5cef757b57560c9f2182a2292ae1130dd
Ivan Shapovalov <intelfx@intelfx.name>
2022-11-19 07:08:43 +0400

    scripts: build.sh: cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
-source ./config
+#!/bin/bash -ex
 
-export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms
+ARGS=( "$@" ); set --
 
-cd $WORKSPACE
+source "${BASH_SOURCE%/*}/config"
+
+export PACKAGES_PATH="$WORKSPACE/edk2:$WORKSPACE/edk2-platforms"
+
+cd "$WORKSPACE"
 . edk2/edksetup.sh
-if [ ! -f "$WORKSPACE/edk2/BaseTools/Source/C/bin" ]; then
-    make -C edk2/BaseTools
+if ! [[ -f edk2/BaseTools/Source/C/bin ]]; then
+    make -j$(nproc) -C edk2/BaseTools
 fi
-build -v -b DEBUG -p i915ovmfPkg/i915ovmf.dsc || exit
+if (( "${#ARGS[@]}" )); then
+    build "${ARGS[@]}"
+else
+    build -v -a X64 -t GCC5 -b DEBUG -p i915ovmfPkg/i915ovmf.dsc
+fi
